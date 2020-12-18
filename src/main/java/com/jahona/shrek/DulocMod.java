60150838c46a4e72977edd3cc5836a56534b8241
jahona <jonah.stockdill@gmail.com>
2020-12-17 20:55:48 -0500

    Initial Import

---
+++ src/main/java/com/jahona/shrek/DulocMod.java
@@ -0,0 +1,35 @@
+package com.jahona.shrek;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("dulocmod")
+public class DulocMod
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public DulocMod() {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
+
+
+
+
+}
